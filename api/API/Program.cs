6491bb28961756687fc9254ea4d37f2833a2da38
bkstech <vishal_varshney@hotmail.com>
2025-11-22 12:33:44 +0530

    running on https

--- api/API/Program.cs
+++ api/API/Program.cs
@@ -72,7 +72,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
                         "http://localhost:3000",
             "http://localhost:3001",
-            "https://localhost:3000",
+            "https://localhost:3443",
             "https://localhost:3001")
               .AllowAnyHeader()
               .AllowAnyMethod()
